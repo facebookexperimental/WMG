d09ebd306f5e16a53710985c5295e4cf07238b93
Matheus Oliveira <matheusmso@fb.com>
2023-08-30 13:30:07 -0300

    updated router lambda with new funcitonality

--- router/index.mjs
+++ router/index.mjs
@@ -3,25 +3,37 @@ AWS.config.update({region: 'sa-east-1'});
 var sqs = new AWS.SQS({apiVersion: '2023-08-09'});
 
 export const lambdaHandler = async (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('ACK for notif'),
-        headers : {'Content-Type':'application/json'},
-    };
+  console.log('incoming request '+JSON.stringify(event));
 
-    console.log('incoming webhoook '+JSON.stringify(event));
-    const queueUrl = process.env.QUEUE_URL;
-    // var queueUrl = 'https://sqs.sa-east-1.amazonaws.com/361535335283/wmg-tasks';
-    var params = {
-        MessageBody: JSON.stringify(event),
-        QueueUrl: queueUrl
-    };
-    sqs.sendMessage(params, function(err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Successfully posted to queue, message id is : ", data.MessageId);
-      }
-    });
-    callback(null, response);
+  // Get the API request from the event
+  const apiRequest = JSON.parse(event.body);
+
+  // Get the endpoint to replay the request to
+  const endpoint = 'graph.facebook.com/v17/' + apiRequest.endpoint;
+
+  // Make the request to the endpoint
+  const client = new AWS.Request('POST', endpoint);
+  client.headers = apiRequest.headers;
+  const response = await client.send();
+
+  const queueUrl = process.env.QUEUE_URL;
+  // var queueUrl = 'https://sqs.sa-east-1.amazonaws.com/361535335283/wmg-tasks';
+  var params = {
+      MessageBody: JSON.stringify(event),
+      QueueUrl: queueUrl
+  };
+  sqs.sendMessage(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Successfully posted to queue, message id is : ", data.MessageId);
+    }
+  });
+
+  // Return the response
+  return {
+    statusCode: response.statusCode,
+    body: response.body
+  };
+  callback(null, response);
 };
