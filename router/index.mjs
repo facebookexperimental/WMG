40265ce771d7bdb285ef82ccfc90e4e9adf7bde2
Carla Teodoro <carlaroteodoro@gmail.com>
2023-08-30 21:02:52 -0300

    Router lambda can write to SQS
    Little refactor of router funciton so that we only enqueue in case of success and we also put code inside a try catch

--- router/index.mjs
+++ router/index.mjs
@@ -1,39 +1,53 @@
 import AWS from 'aws-sdk';
-AWS.config.update({region: 'sa-east-1'});
-var sqs = new AWS.SQS({apiVersion: '2023-08-09'});
 
-export const lambdaHandler = async (event, context, callback) => {
-  console.log('incoming request '+JSON.stringify(event));
+AWS.config.update({ region: 'sa-east-1' });
+const sqs = new AWS.SQS({ apiVersion: '2023-08-09' });
 
-  // Get the API request from the event
-  const apiRequest = JSON.parse(event.body);
+export const lambdaHandler = async (event, context) => {
+    console.log('Incoming request ' + JSON.stringify(event));
 
-  // Get the endpoint to replay the request to
-  const endpoint = 'graph.facebook.com/v17/' + apiRequest.endpoint;
+    try {
+        // Get the API request from the event
+        const apiRequest = JSON.parse(event.body);
 
-  // Make the request to the endpoint
-  const client = new AWS.Request('POST', endpoint);
-  client.headers = apiRequest.headers;
-  const response = await client.send();
+        // Get the endpoint to replay the request to
+        const endpoint = 'graph.facebook.com/v17/' + apiRequest.endpoint;
 
-  const queueUrl = process.env.QUEUE_URL;
-  // var queueUrl = 'https://sqs.sa-east-1.amazonaws.com/361535335283/wmg-tasks';
-  var params = {
-      MessageBody: JSON.stringify(event),
-      QueueUrl: queueUrl
-  };
-  sqs.sendMessage(params, function(err, data) {
-    if (err) {
-      console.log("Error", err);
-    } else {
-      console.log("Successfully posted to queue, message id is : ", data.MessageId);
+        // Make the request to the endpoint
+        const client = new AWS.Request('POST', endpoint);
+        client.headers = apiRequest.headers;
+        const response = await client.send();
+
+        // Check if the HTTP request was successful
+        if (response.statusCode === 200) {
+            console.log("HTTP Request Successful. Status Code:", response.statusCode);
+
+            const queueUrl = process.env.QUEUE_URL;
+            const params = {
+                MessageBody: JSON.stringify(event),
+                QueueUrl: queueUrl
+            };
+
+            // Send a message to the SQS queue
+            const sendMessageResponse = await sqs.sendMessage(params).promise();
+
+            console.log("Successfully enqueued to queue. Message ID:", sendMessageResponse.MessageId);
+        } else {
+            console.error("HTTP Request Failed. Status Code:", response.statusCode);
+        }
+
+        // Return the response
+        return {
+            statusCode: response.statusCode,
+            body: response.body
+        };
+    } catch (error) {
+        console.error("Error:", error);
+
+        // Handle errors and return an appropriate response
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' })
+        };
     }
-  });
-
-  // Return the response
-  return {
-    statusCode: response.statusCode,
-    body: response.body
-  };
-  callback(null, response);
 };
