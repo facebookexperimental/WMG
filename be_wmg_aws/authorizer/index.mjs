d7450f4733e781aab035c2dff6fd4b6db069eef5
Marcus Vinicius Laia De Souza <marcuslaia@meta.com>
2024-07-16 05:59:27 -0700

    Moving auth secret token to Secrets Manager
    Summary:
    **Context:** The WMG-Security-Token is a parameter passed in the headers of every WMG API call to authenticate the user.
    
    **Security issues:** The token is visible on AWS Console in two ways:
    - Environment Variables of AuthFunction Lambda
    - Parameters of CloudFormation
    
    **Solution:**
    - Allocate the security token in Secrets Manager instead of AuthFunction's env
    - Hide the value using NoEcho for CloudFormation parameters
    
    Reviewed By: matheusmso
    
    Differential Revision: D59764172
    
    fbshipit-source-id: 7edec227c0b2e367261a2182cb75315336c2e53b

--- be_wmg_aws/authorizer/index.mjs
+++ be_wmg_aws/authorizer/index.mjs
@@ -3,7 +3,9 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-const SECURITY_TOKEN = process.env.SECURITY_TOKEN;
+import AWS from 'aws-sdk';
+
+const securityTokenSecretArn = process.env.SECURITY_TOKEN_ARN;
 
 export const lambdaHandler = async (event) => {
     console.info('Authorization started', event.methodArn);
@@ -14,6 +16,9 @@ export const lambdaHandler = async (event) => {
     // Create wildcard resource (ref: https://repost.aws/knowledge-center/api-gateway-lambda-authorization-errors)
     const resource = tmp[0] + ":" + tmp[1] + ":" + tmp[2] + ":" + tmp[3] + ":" + tmp[4] + ":" + apiGatewayArnTmp[0] + '/*/*';
 
+    // Get the token from AWS Secrets Manager
+    const securityToken = await getWMGSecurityToken();
+
     // Construct a response which basically will be a policy
     const authResponse = {}
     authResponse.principalId = 'user';
@@ -22,10 +27,30 @@ export const lambdaHandler = async (event) => {
     policyDocument.Statement = [];
     const statement1 = {};
     statement1.Action = 'execute-api:Invoke';
-    statement1.Effect = token === SECURITY_TOKEN ? 'Allow' : 'Deny';
+    statement1.Effect = token === securityToken ? 'Allow' : 'Deny';
     statement1.Resource = resource;
     policyDocument.Statement[0] = statement1;
     authResponse.policyDocument = policyDocument;
 
     return authResponse;
   };
+
+// Helper function to get the Security Token from AWS Secrets Manager
+const getWMGSecurityToken = async () => {
+  try {
+      console.info('Getting WMGSecurityToken');
+      const client = new AWS.SecretsManager();
+      const data = await client.getSecretValue({ SecretId: securityTokenSecretArn }).promise();
+      console.info('Parsing WMGSecurityToken');
+      if ('SecretString' in data) {
+          const secret = JSON.parse(data.SecretString);
+          return secret.WMGSecurityToken;
+      } else {
+          const decodedBinarySecret = Buffer.from(data.SecretBinary, 'base64');
+          return decodedBinarySecret.toString();
+      }
+  } catch (error) {
+      console.error(error);
+      throw error;
+  }
+};
